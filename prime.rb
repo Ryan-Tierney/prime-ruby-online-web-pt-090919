fa000620c884eb06545f76bb49e287e3f47a3f8c
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-10-19 15:56:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,8 @@
 
 def prime?(num)
   return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  Math.sqrt(num).to_i.downto(2).each do |i| return false if num % i == 0
+ end 
   true
 end
 
